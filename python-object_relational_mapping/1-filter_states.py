fb7438b2cb96817c4b5325feb277e0527736aacd
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-06-16 01:35:31 +0200

    Add binary parameter into 1-filter_states query

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     cursor = db_connection.cursor()
 
     cursor.execute(
-        """SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"""
+        """SELECT id, name FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"""
         )
     states = cursor.fetchall()
 
