cd8d81e1243960d06f7cb1b032f6fb1b23d7c201
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-06-29 16:20:46 +0200

    Add 11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+// #!/usr/bin/node
+
+const rawArgs = process.argv.slice(2);
+const numbers = rawArgs.map(Number);
+
+if (numbers <= 1) {
+  console.log(0);
+} else {
+  numbers.sort((a, b) => b - a);
+  console.log(numbers[1]);
+}
