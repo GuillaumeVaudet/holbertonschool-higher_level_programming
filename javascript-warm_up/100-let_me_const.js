77896397dd3695dbd74e7cf25dcdd7b2508647f4
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-06-29 16:59:55 +0200

    Add 100-let_me_const.js and it's testing file called 100-main.js

---
+++ javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// #!/usr/bin/node
+
+if (globalThis.myVar) {
+  globalThis.myVar = 333;
+}
---
+++ javascript-warm_up/100-main.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// #!/usr/bin/node
+
+myVar = 89;
+require('./100-let_me_const')
+console.log(myVar);
