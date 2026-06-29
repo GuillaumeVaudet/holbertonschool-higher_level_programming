432473bfde37c23a8f542f057192a86a88c1566b
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-06-29 16:31:32 +0200

    Add 13-add.js and testing file called it's main.js

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// #!/usr/bin/node
+
+export function add (a, b) {
+  return a + b;
+}
---
+++ javascript-warm_up/main.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// #!/usr/bin/node
+
+const add = require('./13-add').add;
+console.log(add(3, 5));
