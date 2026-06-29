69e99b571330ba8787a0328c503f3008b6037e35
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-06-29 22:04:14 +0200

    Add 2-script with index.html update to test

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('header');
+const red_header = document.getElementById('red_header');
+
+red_header.addEventListener("click", () => {
+    header.classList.add('red')
+})
