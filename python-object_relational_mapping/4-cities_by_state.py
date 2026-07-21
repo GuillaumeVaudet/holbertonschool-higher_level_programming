2ed7d9bfe27e0253fb5f0e3aa87cd053a7dd9c65
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-07-21 04:21:57 +0200

    Fix sql query into 4-cities_by_states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     my_user = sys.argv[1]
     my_password = sys.argv[2]
     my_db = sys.argv[3]
-    searching_value = sys.argv[4]
 
     db_connection = MySQLdb.connect(
         host=my_host,
@@ -27,8 +26,8 @@ if __name__ == "__main__":
 
     my_query = """
                SELECT cities.id, cities.name, states.name FROM states
-                INNER JOIN cities ON cities.states_id = states.id
-                ORDER BY cities.id ASC
+                INNER JOIN cities ON cities.state_id = states.id
+                ORDER BY cities.id ASC;
                """
 
     cursor.execute(my_query)
